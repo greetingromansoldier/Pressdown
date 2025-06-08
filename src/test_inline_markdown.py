de6f1efbbfcfa01cba939178ca5e8326f77f8e8d
greetingromansoldier <greetingromansoldier@proton.me>
2025-06-08 16:46:43 +0300

    complete tests for extracting images/links from inline markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -130,9 +130,10 @@ class TestInlineMarkdown(unittest.TestCase):
 
     # == Split links and images from text to TextNodes ==
 
-    def test_split_images(self):
+    def test_split_images_normal_case(self):
         node = TextNode(
-            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+            ("This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)" 
+            " and another ![second image](https://i.imgur.com/3elNhQu.png)"),
             TextType.TEXT,
         )
         new_nodes = split_nodes_image([node])
@@ -150,13 +151,15 @@ class TestInlineMarkdown(unittest.TestCase):
 
     def test_split_links_normal_case(self):
         node = TextNode(
-            "This is text with a link [to google](https://www.google.com) and [to youtube](https://www.youtube.com)",
+            ("This is a text with link [to google](https://www.google.com)"
+              " and [to youtube](https://www.youtube.com)"
+             ),
             TextType.TEXT,
         )
         new_nodes = split_nodes_link([node])
         self.assertListEqual(
             [
-                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("This is a text with link ", TextType.TEXT),
                 TextNode("to google", TextType.LINK, "https://www.google.com"),
                 TextNode(" and ", TextType.TEXT),
                 TextNode(
@@ -164,5 +167,140 @@ class TestInlineMarkdown(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+
+    # == If no links or images in text - must return just same text node with same type ==
+
+    def test_no_links(self):
+        node = TextNode(
+            ("This is text without links"),
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode(
+                ("This is text without links"),
+                TextType.TEXT,
+                )
+            ],
+            new_nodes
+        )
+
+    def test_no_images(self):
+        node = TextNode(
+            ("This is text without images"),
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode(
+                ("This is text without images"),
+                TextType.TEXT,
+                )
+            ],
+            new_nodes
+        )
+
+    def test_images_on_corners(self):
+        node = TextNode(
+            ("![image](https://i.imgur.com/zjjcJKZ.png)" 
+            " and another ![second image](https://i.imgur.com/3elNhQu.png)"),
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and another ", TextType.TEXT),
+                TextNode(
+                    "second image", TextType.IMAGE, "https://i.imgur.com/3elNhQu.png"
+                ),
+            ],
+            new_nodes,
+        )
+    
+    def test_links_on_corners(self):
+
+        node = TextNode(
+            ("[to google](https://www.google.com)"
+              " and [to youtube](https://www.youtube.com)"
+             ),
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("to google", TextType.LINK, "https://www.google.com"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com"
+                ),
+            ],
+            new_nodes,
+        )
+
+    def test_image_no_text(self):
+
+        node = TextNode(
+            "![image](https://i.imgur.com/zjjcJKZ.png)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+            ],
+            new_nodes,
+        )
+
+    def test_link_no_text(self):
+        node = TextNode(
+            "[to google](https://www.google.com)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("to google", TextType.LINK, "https://www.google.com"),
+            ],
+            new_nodes,
+        )
+
+    def test_invalid_markdown_image(self):
+        node = TextNode(
+            "here's some text and invalid image syntax "
+            "!(image)[https://i.imgur.com/zjjcJKZ.png]",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+            TextNode(
+            "here's some text and invalid image syntax "
+            "!(image)[https://i.imgur.com/zjjcJKZ.png]",
+            TextType.TEXT,
+        )
+            ],
+            new_nodes
+        )
+
+    def test_invalid_markdown_link(self):
+        node = TextNode(
+            "here's some text and invalid link syntax "
+            "(to google)[https://www.google.com]",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_image([node])
+        self.assertListEqual(
+            [
+            TextNode(
+            "here's some text and invalid link syntax "
+            "(to google)[https://www.google.com]",
+            TextType.TEXT,
+        )
+            ],
+            new_nodes
         )
 
