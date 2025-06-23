2c485282afadea7d23d2502e84aec5b9f230de8f
greetingromansoldier <greetingromansoldier@proton.me>
2025-06-23 10:02:47 +0300

    add some start functions (markdown paragraph/headings to html nodes)

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -14,15 +14,19 @@ from block_markdown import (
     block_to_block_type
 )
 
+# for now I not only do code for nodes but also use node.to_html()
+# for representing results to myself and checking if it works like it supposed to
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
     for block in blocks:
         if block_to_block_type(block) == BlockType.PARAGRAPH:
             print(f"'{block}'\nis paragraph")
-            print(f"Results of to_html func:\n{block_is_paragraph(block)}")
+            print(f"Results of to_html func:\n{markdown_paragraph_to_html_node(block)}")
             print("*"*50)
         if block_to_block_type(block) == BlockType.HEADING:
             print(f"'{block}'\nis heading")
+            print(f"Results of to_html func:\n"+
+                  f"{markdown_heading_to_html_node(block)}")
             print("*"*50)
         if block_to_block_type(block) == BlockType.CODE:
             print(f"'{block}'\nis code")
@@ -38,14 +42,43 @@ def markdown_to_html_node(markdown):
             print("*"*50)
     return blocks
     
-def block_is_paragraph(block):
-    node = LeafNode("p", None, block)
-    node.to_html()
-    return(node)
+def markdown_paragraph_to_html_node(block):
+    node = LeafNode(tag="p", value=block, props=None)
+    html_text = node.to_html()
+    return html_text # making it text only for the test representation
+
+def markdown_heading_to_html_node(block):
+    # find a way to count # in markdown headings and turn into h1-h6 for html
+    heading_value = 0
+    block_value = ""
+    if block.startswith("# "):
+        heading_value = 1
+        block_value = block.strip().split("#")[1].strip()
+    elif block.startswith("## "):
+        heading_value = 2
+        block_value = block.strip().split("##")[1].strip()
+    elif block.startswith("### "):
+        heading_value = 3
+        block_value = block.strip().split("###")[1].strip()
+    elif block.startswith("#### "):
+        heading_value = 4
+        block_value = block.strip().split("####")[1].strip()
+    elif block.startswith("##### "):
+        heading_value = 5
+        block_value = block.strip().split("#####")[1].strip()
+    elif block.startswith("###### "):
+        heading_value = 6
+        block_value = block.strip().split("######")[1].strip()
+    node = LeafNode(tag=f'h{heading_value}', value=block_value, props=None)
+    return node.to_html()
 
 
 md = """
 Hello
+
+# Heading
+
+## Heading 2
 
 >THis is block with *italic* text
 >And just another text in one block
@@ -57,4 +90,4 @@ with **bolded** text
 markdown_to_html_node(md)
 
 # ok. I am overwhelmed and unerstand kind of NOTHING
-# and it's the time to prepare food for a weak
+# and it's the time to prepare food for a week
