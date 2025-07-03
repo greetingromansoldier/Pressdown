6d5ddd015c37553bcccf6f3a16db2657a534f917
greetingromansoldier <greetingromansoldier@proton.me>
2025-07-03 10:13:11 +0300

    finish the main logic for transforming full md text to full html node

--- src/markdown_to_html_node.py
+++ src/markdown_to_html_node.py
@@ -16,76 +16,35 @@ from block_markdown import (
     markdown_to_blocks, 
     block_to_block_type
 )
-import textnode
 
-# for now I not only do code for nodes but also use node.to_html()
-# for representing results and checking if it works as intended
 def markdown_to_html_node(markdown):
     blocks = markdown_to_blocks(markdown)
+    nodes = []
     for block in blocks:
         if block_to_block_type(block) == BlockType.PARAGRAPH:
-            print(f"'{block}'\nis paragraph")
-            print(f"Results of to_html func:\n{markdown_paragraph_to_html_node(block)}")
-            print("*"*50)
+            nodes.append(markdown_paragraph_to_html_node(block))
         if block_to_block_type(block) == BlockType.HEADING:
-            print(f"'{block}'\nis heading")
-            print(f"Results of to_html func:\n"+
-                  f"{markdown_heading_to_html_node(block)}")
-            print("*"*50)
+            nodes.append(markdown_heading_to_html_node(block))
         if block_to_block_type(block) == BlockType.CODE:
-            print(f"'{block}'\nis code")
-            print(f"code block transformed to html:\n"
-                  +f"|{markdown_code_block_to_html_node(block)}|"
-                  )
-            print("*"*50)
+            nodes.append(markdown_code_block_to_html_node(block))
         if block_to_block_type(block) == BlockType.QUOTE:
-            print(f"'{block}'\nis quote")
-            print(f"quote blockk transformed to html:\n"
-                  +f"{markdown_quote_block_to_html_node(block)}"
-                  )
-            print("*"*50)
+            nodes.append(markdown_quote_block_to_html_node(block))
         if block_to_block_type(block) == BlockType.UNORDERED_LIST:
-            print(f"'{block}'\nis unordered list")
-            print(f"unordered list transformed to html:\n"
-                  +f"{markdown_unordered_list_to_html_node(block)}"
-                  )
-            print("*"*50)
+            nodes.append(markdown_unordered_list_to_html_node(block))
         if block_to_block_type(block) == BlockType.ORDERED_LIST:
-            print(f"'{block}'\nis ordered list")
-            print(f"unordered list transformed to html:\n"
-                  +f"{markdown_ordered_list_to_html_node(block)}"
-                  )
-            print("*"*50)
-    return blocks
+            nodes.append(markdown_ordered_list_to_html_node(block))
+
+    div_node = ParentNode(tag="div", children=nodes)
+    return div_node.to_html()
     
 def markdown_paragraph_to_html_node(block):
     children = block_to_children(block)
     parent = ParentNode(tag="p", children=children)
-    return parent.to_html()
+    return parent
 
 def markdown_heading_to_html_node(block):
-    heading_value = 0
-    block_value = ""
-    if block.startswith("# "):
-        heading_value = 1
-        block_value = block.strip().split("#")[1].strip()
-    elif block.startswith("## "):
-        heading_value = 2
-        block_value = block.strip().split("##")[1].strip()
-    elif block.startswith("### "):
-        heading_value = 3
-        block_value = block.strip().split("###")[1].strip()
-    elif block.startswith("#### "):
-        heading_value = 4
-        block_value = block.strip().split("####")[1].strip()
-    elif block.startswith("##### "):
-        heading_value = 5
-        block_value = block.strip().split("#####")[1].strip()
-    elif block.startswith("###### "):
-        heading_value = 6
-        block_value = block.strip().split("######")[1].strip()
-    node = LeafNode(tag=f'h{heading_value}', value=block_value, props=None)
-    return node.to_html()
+    children = block_to_children(block)
+    return children
 
 def markdown_code_block_to_html_node(block):
     text_value = ""
@@ -95,25 +54,22 @@ def markdown_code_block_to_html_node(block):
     text_node = TextNode(text=text_value, text_type=TextType.CODE)
     internal_html = text_node_to_html_node(text_node)
     parent_node = ParentNode(tag="pre", children=[internal_html])
-    return parent_node.to_html()
-
-    # quote_block_parent = ParentNode(tag="pre", children=children)
-    # return quote_block_parent.to_html()
+    return parent_node
 
 def markdown_quote_block_to_html_node(block):
     children = block_to_children(block)
     quote_block_parent = ParentNode(tag="blockquote", children=children)
-    return quote_block_parent.to_html()
+    return quote_block_parent
 
 def markdown_unordered_list_to_html_node(block):
     children = block_to_children(block)
     unordered_list_parent = ParentNode(tag="ul", children=children)
-    return unordered_list_parent.to_html()
+    return unordered_list_parent
 
 def markdown_ordered_list_to_html_node(block):
     children = block_to_children(block)
     unordered_list_parent = ParentNode(tag="ol", children=children)
-    return unordered_list_parent.to_html()
+    return unordered_list_parent
 
 def block_to_children(block):
     if block_to_block_type(block) == BlockType.PARAGRAPH:
@@ -122,6 +78,31 @@ def block_to_children(block):
         for node in text_nodes:
             html_nodes.append(text_node_to_html_node(node))
         return html_nodes
+
+    elif block_to_block_type(block) == BlockType.HEADING:
+        heading_value = 0
+        block_value = ""
+        if block.startswith("# "):
+            heading_value = 1
+            block_value = block.strip().split("#")[1].strip()
+        elif block.startswith("## "):
+            heading_value = 2
+            block_value = block.strip().split("##")[1].strip()
+        elif block.startswith("### "):
+            heading_value = 3
+            block_value = block.strip().split("###")[1].strip()
+        elif block.startswith("#### "):
+            heading_value = 4
+            block_value = block.strip().split("####")[1].strip()
+        elif block.startswith("##### "):
+            heading_value = 5
+            block_value = block.strip().split("#####")[1].strip()
+        elif block.startswith("###### "):
+            heading_value = 6
+            block_value = block.strip().split("######")[1].strip()
+        heading_node = LeafNode(tag=f'h{heading_value}', value=block_value, props=None)
+        return heading_node
+
 
     elif block_to_block_type(block) == BlockType.QUOTE:
         block_values = []
@@ -169,7 +150,7 @@ def block_to_children(block):
 
 
 
-
+# will be transfered to tests
 md = """
 Hello my little friend. I want some _italic_ text here. This would be awesome. 
 That's a good thing to add **bold text** also, and of course some [image](http://link to an image)
@@ -180,7 +161,7 @@ That's a good thing to add **bold text** also, and of course some [image](http:/
 
 ####### Heading ???
 
->THis is quote block with *italic* text
+>THis is quote block with _italic_ text
 >And just another quote text in one block
 
 And here's the much much better block
@@ -204,10 +185,11 @@ with **bolded** text
 
 ```
 and finally we are **doing** codeblock
+and some other stuff
 ```
 """
 
-markdown_to_html_node(md)
+html_page = markdown_to_html_node(md)
+print(html_page)
 
-# ok. I am overwhelmed and unerstand kind of NOTHING
-# and it's the time to prepare food for a week
+
