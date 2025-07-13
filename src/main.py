82ce82b4b59e898f89cc2dbb572eff2e75179c02
greetingromansoldier <greetingromansoldier@proton.me>
2025-07-13 12:03:19 +0300

    first working version of recursion func to copy files to public

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@ from textnode import TextNode
 def main():
     test_obj = TextNode("Some Text", "link", "https://www.google.com")
     print(test_obj.__repr__())
+    
     current_directory = os.getcwd()
     print(f"current_directory {current_directory}")
     file_abs_path = os.path.abspath(__file__)
@@ -15,19 +16,33 @@ def main():
     print(f"probably path to directory where executed file is:{project_dir_name}")
 
     public_path = f"{project_dir_name}/public"
-    static_files = os.listdir(path=f"{project_dir_name}/static")
+    print(f"public_path: {public_path}")
+    static_path = f"{project_dir_name}/static"
+    print(f"static_path: {static_path}")
 
-    print(static_files)
-    for elem in static_files:
-        print(elem)
-        if os.path.isfile(f"{project_dir_name}/static/{elem}"):
-            print(f"printing elem because it's file {elem}")
-    
     if os.path.exists(public_path):
         shutil.rmtree(path=public_path) 
         os.mkdir(public_path)
     else: 
         os.mkdir(public_path)
+
+    def copy_recursive(path: str, dst: str):
+        print(f"current path:{path}")
+        new_path, new_dst = "", ""
+        for elem in os.listdir(path=path):
+            print(f"list directory at path:\n{os.listdir(path=path)}")
+            print(f"elem: {elem}")
+            if os.path.isfile(f"{path}/{elem}"):
+                print(f"printing elem because it's file {elem}")
+                shutil.copy(src=f"{path}/{elem}", dst=f"{dst}/{elem}")
+            else:
+                os.mkdir(path=f"{dst}/{elem}")
+                new_path, new_dst = copy_recursive(f"{path}/{elem}", f"{dst}/{elem}")
+        return new_path, new_dst
+
+    copy_recursive(path=static_path, dst=public_path)
+
+        
 
 
 if __name__ == "__main__":
