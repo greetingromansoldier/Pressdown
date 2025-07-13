dc2f836257690c6612a8ab2c32e352c1975238dd
greetingromansoldier <greetingromansoldier@proton.me>
2025-07-13 18:18:21 +0300

    solve some bug with images/links, complete the main project

--- src/text_node_to_html_node.py
+++ src/text_node_to_html_node.py
@@ -11,8 +11,8 @@ def text_node_to_html_node(text_node):
     if text_node.text_type is TextType.CODE:
         return LeafNode("code", text_node.text)
     if text_node.text_type is TextType.LINK:
-        return LeafNode("a", text_node.text, {"href":"some link here"})
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type is TextType.IMAGE:
-        return LeafNode("img", "", {"src": "source", 
-                                    "alt":"alt text"})
+        return LeafNode("img", "", {"src": text_node.url, 
+                                    "alt": text_node.text})
     raise ValueError(f"Invalid TextType {text_node.text_type}")
